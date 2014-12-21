4c527302fab89148e261d30b9c208cc649781dfb
Andreas Orthey <andreas.orthey@gmx.de>
2014-12-21 13:07:34 +0100

    added waist/hip/foot in line assumption

--- main-cspace-sampler.py
+++ main-cspace-sampler.py
@@ -19,17 +19,28 @@ q = np.array((0.0,0.0,0.0,0.0,0.0))
 #q = np.array((-pi/16,+pi/16,-pi/4,pi/16,0.8))
 
 
+## real limits from the hardware
 qL = np.array((-1.31,-0.03,-2.18,-0.09,-0.52))
 qU = np.array((0.73,2.62,0.73,1.05,0.79))
 
+## artificial limits imposed by tangens
+tlimit = pi/3
+qaL = np.array((-tlimit,-tlimit,-tlimit,-tlimit,-tlimit))
+qaU = np.array((tlimit,tlimit,tlimit,tlimit,tlimit))
+
 ##generate uniform random config qL <= q <= qU
-Nsamples=1000
+Nsamples=100000
+qarray=[]
 for ctr in range(0,Nsamples):
         for i in range(0,len(qL)):
                 q[i]=rnd.uniform(qL[i], qU[i])
 
         if not((q<=qU).all() and (q>=qL).all()):
-                print "not feasible configuration"
+                #print "not feasible configuration"
+                continue
+        if not((q<=qaU).all() and (q>=qaL).all()):
+                #print "not in range of tan configuration"
+                continue
         ## compute q -> x
 
         Npts = 40
@@ -67,6 +78,7 @@ for ctr in range(0,Nsamples):
         #### knee-to-hip path
         ################################################################################
         offset = knee_height*t0
+        kneepos = offset
         while heights[xctr] < hip_height:
                 x[xctr] = (heights[xctr]-knee_height)*t1+offset
                 xctr=xctr+1
@@ -76,8 +88,7 @@ for ctr in range(0,Nsamples):
         ################################################################################
 
         offset = knee_height*t0+(hip_height-knee_height)*t1
-        if abs(offset) >= 0.02:
-                continue
+        hippos = offset
 
         while heights[xctr] < waist_height:
                 x[xctr] = (heights[xctr]-hip_height)*t2+offset
@@ -90,6 +101,8 @@ for ctr in range(0,Nsamples):
                         +(hip_height-knee_height)*t1\
                         +(waist_height-hip_height)*t2
 
+        waistpos = offset
+
         while heights[xctr] < neck_height:
                 x[xctr] = (heights[xctr]-waist_height)*t3+offset
                 xctr=xctr+1
@@ -100,6 +113,7 @@ for ctr in range(0,Nsamples):
                         +(hip_height-knee_height)*t1\
                         +(waist_height-hip_height)*t2\
                         +(neck_height-waist_height)*t3
+        neckpos = offset
 
 
         while xctr<len(heights) and heights[xctr] < head_height:
@@ -111,11 +125,16 @@ for ctr in range(0,Nsamples):
                         +(waist_height-hip_height)*t2\
                         +(neck_height-waist_height)*t3\
                         +(head_height-neck_height)*t4
-        if abs(headpos) >= 0.1:
+
+        if abs(headpos) >= 0.05:
+                continue
+        if abs(hippos) >= 0.005:
+                continue
+        if abs(waistpos) >= 0.005:
                 continue
 
-
-        ## display x
+        qarray.append(q)
+        ### display x
         fig=figure(1)
         fig.clf()
         ax = fig.gca()
@@ -125,10 +144,14 @@ for ctr in range(0,Nsamples):
         plot(x,y,'-r')
         lenlines=0.6
         plot([-lenlines,lenlines],[head_height,head_height],'-k',linewidth=3.0)
-        #plot([-lenlines,lenlines],[hip_height,hip_height],'-k',linewidth=2.0)
+        plot([-lenlines,lenlines],[neck_height,neck_height],'-k',linewidth=2.0)
+        plot([-lenlines,lenlines],[waist_height,waist_height],'-k',linewidth=2.0)
+        plot([-lenlines,lenlines],[hip_height,hip_height],'-k',linewidth=2.0)
+        plot([-lenlines,lenlines],[knee_height,knee_height],'-k',linewidth=2.0)
 
         for i in range(0,len(heights)):
                 plot([-lenlines,lenlines],[heights[i],heights[i]],'-b')
+        #print q
         plt.pause(0.0001)
 
-
+print len(qarray),"samples from Q (from",Nsamples,"random shots)"
