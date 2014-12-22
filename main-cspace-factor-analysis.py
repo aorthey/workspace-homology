fixed q4 computation

main-cspace-factor-analysis.py
@@ -1,5 +1,5 @@
 import random as rnd
-from math import cos,sin,tan,pi,asin,acos,atan2
+from math import cos,sin,tan,pi,asin,acos,atan2,atan
 import pickle
 import numpy as np
 from src.robotspecifications import *
@@ -26,7 +26,7 @@ qaU = np.array((tlimit,tlimit,tlimit,tlimit,tlimit))
 
 ###############################################################################
 ## X \in \R^Npts, the space of points, each constraint to one environment (E) box
-Npts = 40
+Npts = 30
 
 ## L is the height of the head, which we fix here to investigate the factors
 ## which define the surface which induces the same linear subspace in X
@@ -35,7 +35,7 @@ Xarray = []
 
 
 ##iterate over homotopy classes (HC)
-for k in range(0,4):
+for k in range(0,1):
         asign = 1
         bsign = 1
         if k==0:
@@ -51,7 +51,7 @@ for k in range(0,4):
                 asign=-1
                 bsign=1
 
-        L = 1.4
+        L = 1.3
         h1 = 0.0
         hmin=1000
         hmax=0
@@ -70,9 +70,21 @@ for k in range(0,4):
                 ## b : distance from neck to the main axis through foot, hip and waist.
 
                 ## http://mathworld.wolfram.com/Circle-CircleIntersection.html
+                h3 = 0.05
                 h2 = L - h1 - d2
                 a=asign*0.5*(1/h1)*sqrt(4*h1*h1*d0*d0-pow(h1*h1-d1*d1+d0*d0,2)) 
-                b=bsign*0.5*(1/h2)*sqrt(4*h2*h2*d3*d3-pow(h2*h2-d4*d4+d3*d3,2)) 
+                d5 = sqrt(h3*h3+h2*h2)
+                b=bsign*0.5*(1/d5)*sqrt(4*d5*d5*d3*d3-pow(d5*d5-d4*d4+d3*d3,2)) 
+
+                if math.isnan(b):
+                        print "triangle inequality d3+d4 > d5 is not fulfilled"
+                        print d3,d4,d5
+                        continue
+                if math.isnan(a):
+                        print "triangle inequality d0+d1 > dist(foot,hip) is not fulfilled"
+                        print d0,d1
+                        continue
+                ####
 
                 qh1 = np.array((0.0,0.0,0.0,0.0,0.0))
                 if abs(-a/d0) > 1 or abs(a/d1) > 1:
@@ -83,12 +95,12 @@ for k in range(0,4):
                 qh1[1] = asin(a/d1)
                 qh1[2] = 0.0
 
-                if abs(-b/d3) > 1 or abs(b/d4) > 1:
-                        print "fatal error: knee must be below foot, not allowed"
-                        continue
+                qh1[3] = asin(h3/d5)+asin(b/d3)
+                v = np.array((h3-d3*sin(qh1[3]),h2-d3*cos(qh1[3])))
+                zaxis = np.array((0,1))
+                vn = v/np.linalg.norm(v)
 
-                qh1[3] = asin(-b/d3)
-                qh1[4] = asin(b/d4)
+                qh1[4]=acos(np.dot(zaxis,vn))*vn[0]/abs(vn[0])
 
                 q = qh1
                 ###############################################################################
@@ -96,9 +108,10 @@ for k in range(0,4):
                 ###############################################################################
 
                 if not((q<=qaU).all() and (q>=qaL).all()):
-                        #print "not in range of tan configuration"
+                        print "not in range of tan configuration",q
                         continue
-                if h1 < hmin:
+
+                if h1 <= hmin:
                         hmin = h1
                 if h1 >= hmax:
                         hmax = h1
@@ -203,9 +216,8 @@ for k in range(0,4):
                 plot([0,-a],[0,knee_height],'-b',linewidth=5.0)
                 plot([-a,0],[knee_height,hip_height],'-b',linewidth=5.0)
                 plot([0,0],[hip_height,waist_height],'-b',linewidth=5.0)
-                plot([0,-b],[waist_height,neck_height],'-b',linewidth=5.0)
-                plot([-b,headpos],[neck_height,head_height],'-b',linewidth=5.0)
-                print q[4]
+                plot([0,sin(q[3])*d3],[waist_height,neck_height],'-b',linewidth=5.0)
+                plot([sin(q[3])*d3,headpos],[neck_height,head_height],'-b',linewidth=5.0)
 
                 y=heights
                 ax.scatter(x,y,marker='o',c='r')